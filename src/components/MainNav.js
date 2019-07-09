ce9c4e163e110d6966acc848181d9c493e85e3b7
amlane <twelvexstring@gmail.com>
2019-07-09 18:30:55 -0500

    my awesome super descriptive commmit message about how I added an example.html file

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -3,21 +3,23 @@ import { Link } from "react-router-dom";
 
 import "./MainNav.scss";
 
-function MainNav() {
-  return (
-    <header className="main-navigation">
-      <h1 className="title">Your Website</h1>
-      <nav className="mn-link-cta">
-        <Link to="/" className="slide-left one">
-          Home
-        </Link>{" "}
-        <Link className="slide-left two">Contact Us</Link>{" "}
-        <Link to="/about" className="slide-left three">
-          About
-        </Link>{" "}
-      </nav>
-    </header>
-  );
+class MainNav extends React.Component {
+  render() {
+    return (
+      <header className="main-navigation">
+        <h1 className="title">Your Website</h1>
+        <nav className="mn-link-cta">
+          <Link to="/" className="slide-left one">
+            Home
+          </Link>{" "}
+          <Link className="slide-left two">Contact Us</Link>{" "}
+          <Link to="/about" className="slide-left three">
+            About
+          </Link>{" "}
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default MainNav;
